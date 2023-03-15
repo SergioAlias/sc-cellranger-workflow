5e6c25e97a6eb62eccbcd792749173677cc0a746
SergioAlias <sergio08071998@gmail.com>
2023-03-15 15:59:38 +0100

    Fixed mkfastq script

--- test-mkfastq.sh
+++ test-mkfastq.sh
@@ -5,8 +5,11 @@
 # Test code for Cell Ranger mkfast
 # rc-i2-override for solving the I2 issue (version < 7.0.1)
 
+# export PATH=~sergioalias/cellranger-7.1.0:$PATH
+echo `which cellranger`
 
-cellranger mkfastq --id=tiny-bcl \
+
+cellranger mkfastq --id=tiny-bcl-7-0-0 \
                    --run=cellranger-tiny-bcl-1.2.0 \
                    --csv=cellranger-tiny-bcl-simple-1.2.0.csv #\
                    # --rc-i2-override=true
