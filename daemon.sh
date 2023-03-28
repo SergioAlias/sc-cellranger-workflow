ca64ab0c0760684c93e6d5bd9907516e0be5fffa
SergioAlias <sergio08071998@gmail.com>
2023-03-28 11:59:59 +0200

    Added daemon config file

--- daemon.sh
+++ daemon.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # Sergio Alías, 20230323
-# Last modified 20230323
+# Last modified 20230328
 
 # daemon.sh
 
@@ -14,18 +14,18 @@ module=$1
 ## STAGE EXECUTION
 #######################################################################
 
-if [ "$module" == "1" ] ; then
-	#STAGE 1 CONVERTING BCL FILES INTO FASTQ
-	echo "Launching stage 1: Converting BCL files into FASTQ"
+if [ "$module" == "0" ] ; then
+	#STAGE 0 CONVERTING BCL FILES INTO FASTQ
+	echo "Launching stage 0: Converting BCL files into FASTQ"
 	if [ $launch_login == TRUE ]; then	
 		cellranger_mkfastq.sh
 	else
 		sbatch cellranger_mkfastq.sh
 	fi
 
-elif [ "$module" == "2" ] ; then
-	#STAGE 2 OBTAINING COUNTS FROM FASTQ FILES
-	echo "Launching stage 2: Obtaining counts from FASTQ files"
+elif [ "$module" == "1" ] ; then
+	#STAGE 1 OBTAINING COUNTS FROM FASTQ FILES
+	echo "Launching stage 1: Obtaining counts from FASTQ files"
 	if [ $launch_login == TRUE ]; then	
 		cellranger_count.sh
 	else
