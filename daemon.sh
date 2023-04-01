bdb15783ab7ed53f74a7556f83687b8744b70d1a
SergioAlias <sergio08071998@gmail.com>
2023-04-01 12:05:39 +0200

    Updated daemon

--- daemon.sh
+++ daemon.sh
@@ -1,14 +1,16 @@
 #! /usr/bin/env bash
 
 # Sergio Alías, 20230323
-# Last modified 20230328
+# Last modified 20230401
 
 # daemon.sh
 
 # Script for controlling the single-cell Cell Ranger workflow
 
 
+CONFIG_DAEMON='config_daemon'
 module=$1
+source $CONFIG_DAEMON
 
 
 ## STAGE EXECUTION
@@ -18,16 +20,16 @@ if [ "$module" == "0" ] ; then
 	#STAGE 0 CONVERTING BCL FILES INTO FASTQ
 	echo "Launching stage 0: Converting BCL files into FASTQ"
 	if [ $launch_login == TRUE ]; then	
-		cellranger_mkfastq.sh
+		$aux_folder/cellranger_mkfastq.sh $experiment_name
 	else
-		sbatch cellranger_mkfastq.sh
+		sbatch $aux_folder/cellranger_mkfastq.sh $experiment_name
 	fi
 
 elif [ "$module" == "1" ] ; then
 	#STAGE 1 OBTAINING COUNTS FROM FASTQ FILES
 	echo "Launching stage 1: Obtaining counts from FASTQ files"
 	if [ $launch_login == TRUE ]; then	
-		cellranger_count.sh
+		$aux_folder/cellranger_count.sh $experiment_name
 	else
-		sbatch cellranger_count.sh
+		sbatch $aux_folder/cellranger_count.sh $experiment_name
 	fi
