d644031115ce78498bf3f814cc125d030a00c77f
SergioAlias <sergio08071998@gmail.com>
2023-05-17 16:37:42 +0200

    Inplemented Autoflow for Cellranger count

--- aux_sh/autoflow_count.sh
+++ aux_sh/autoflow_count.sh
@@ -1,5 +1,5 @@
 # Sergio Alías, 20230516
-# Last modified 20230516
+# Last modified 20230517
 
 # STAGE 1 OBTAINING COUNTS FROM FASTQ FILES
 
@@ -7,5 +7,11 @@
 
 
 while IFS= read sample; do
-AutoFlow -w $TEMPLATE -o "$COUNT_RESULTS_FOLDER"/"$sample" "$RESOURCES"
+    
+    AF_VARS=`echo "
+    \\$sample=$sample
+    " | tr -d [:space:]`
+
+    AutoFlow -w $TEMPLATE -V "$AF_VARS" -o "$COUNT_RESULTS_FOLDER"/"$sample" "$RESOURCES"
+
 done < $SAMPLES_FILE
