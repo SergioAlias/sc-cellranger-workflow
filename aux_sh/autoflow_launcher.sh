38332fefb0e44e3d65248289d8c8a865fc17db8c
SergioAlias <sergio08071998@gmail.com>
2023-06-12 14:26:15 +0200

    re-organized Autoflow

---
+++ aux_sh/autoflow_launcher.sh
@@ -0,0 +1,29 @@
+# Sergio Alías, 20230516
+# Last modified 20230612
+
+# Generic Autoflow launcher
+
+. ~soft_bio_267/initializes/init_autoflow
+
+
+if ["$1" == "count"] ; then # STAGE 1 OBTAINING COUNTS FROM FASTQ FILES
+    export RESULTS_FOLDER=$COUNT_RESULTS_FOLDER
+    export TEMPLATE=$COUNT_TEMPLATE
+elif ["$1" == "qc"] ; then # STAGE 2 QUALITY CONTROL AND TRIMMING
+    export RESULTS_FOLDER=$QC_RESULTS_FOLDER
+    export TEMPLATE=$QC_TEMPLATE	
+fi
+
+mkdir -p $RESULTS_FOLDER
+
+PATH=$LAB_SCRIPTS:$PATH
+
+while IFS= read sample; do
+    
+    AF_VARS=`echo "
+    \\$sample=$sample
+    " | tr -d [:space:]`
+
+    AutoFlow -w $TEMPLATE -V "$AF_VARS" -o $RESULTS_FOLDER/$sample #$RESOURCES
+
+done < $SAMPLES_FILE
--- daemon.sh
+++ daemon.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # Sergio Alías, 20230323
-# Last modified 20230605
+# Last modified 20230612
 
 # daemon.sh
 
@@ -22,7 +22,7 @@ export PATH=$CODE_PATH'/aux_sh:'$PATH
 #######################################################################
 
 if [ "$module" == "0" ] ; then
-    #STAGE 0 CONVERTING BCL FILES INTO FASTQ
+    # STAGE 0 CONVERTING BCL FILES INTO FASTQ
     echo "Launching stage 0: Converting BCL files into FASTQ"
     if [ $launch_login == TRUE ]; then  
         cellranger_mkfastq.sh
@@ -31,17 +31,17 @@ if [ "$module" == "0" ] ; then
     fi
 
 elif [ "$module" == "1" ] ; then
-    #STAGE 1 OBTAINING COUNTS FROM FASTQ FILES
+    # STAGE 1 OBTAINING COUNTS FROM FASTQ FILES
     echo "Launching stage 1: Obtaining counts from FASTQ files"
-    # if [ $launch_login == TRUE ]; then  
-    #     cellranger_count.sh
-    # else
-    #     sbatch cellranger_count.sh
-    # fi
-    autoflow_count.sh
+    autoflow_launcher.sh count
 
-elif [ "$module" == "2" ] ; then
-    #STAGE 2 SAMPLES COMPARISON
+elif [ "$module" == "2a" ] ; then
+    # STAGE 2 QUALITY CONTROL AND TRIMMING
+    echo "Launching stage 2: Quality control and trimming"
+    autoflow_launcher.sh qc
+
+elif [ "$module" == "2b" ] ; then
+    # STAGE 2 SAMPLES COMPARISON
     echo "Launching stage 2: Samples comparison"
     if [ $launch_login == TRUE ]; then  
         compare_samples.sh
@@ -50,7 +50,7 @@ elif [ "$module" == "2" ] ; then
     fi
 
 elif [ "$module" == "3" ] ; then
-    #STAGE 3 PREPROCESSING
+    # STAGE 3 PREPROCESSING
     echo "Launching stage 3: Preprocessing"
     if [ $launch_login == TRUE ]; then  
         preprocess.sh
